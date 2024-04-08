6a52fe040b57d4ecc6526cb4406c53eeb2cb7492
Nitesh Marwaha <97465378+NiteshMarwaha@users.noreply.github.com>
2024-04-08 13:49:29 +0530

    Add files via upload

---
+++ section/Insights.jsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn } from '../utils/motion';
+import { InsightCard, TitleText, TypingText } from '../components';
+import { insights } from '../constants';
+
+const Insights = () => (
+  <section className={`${styles.paddings} ${styles.flexCenter} flex-col relative z-10`}>
+    <TypingText title="|Insight" />
+    <TitleText title="Insight about metaverse" />
+    <motion.div variants={fadeIn('up', 'tween', 0.3, 1)}
+      whileInView="show"
+      initial="hidden"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className="flex flex-col justify-center justify-items-center content-center  gap-4 mt-[60px] "
+
+    >
+      {
+        insights.map((insight, index) => <InsightCard key={index} index={index} {...insight} />)
+      }
+
+    </motion.div>
+
+  </section>
+);
+
+export default Insights;
